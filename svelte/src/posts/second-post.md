9bf7b1c39034807530cae2448e646ba3b4f330fa
joe senenfelder <est2020.joe@gmail.com>
2023-05-27 13:15:14 -0500

    posts api endpoint

---
+++ svelte/src/posts/second-post.md
@@ -0,0 +1,27 @@
+---
+title : Second post
+description: Second post.
+date: '2023-5-27'
+categories:
+    - sveltekit
+    - svelte
+published: true
+---
+
+
+## Svelte
+
+Media inside the **static** folder is served from `/`.
+
+![Svelte](favicon.png)
+
+
+## Markddown
+
+Hey Friends - thanks to Joy of Code 
+
+```ts
+function greet (name: string) {
+    console.log('Hey ${name}!')
+}
+```
--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -1,22 +1,19 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
-import {mdsvex} from 'mdsvex'
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extension: ['.md'],
+	extensions: ['.md'],
 }
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-
+	extensions: ['.svelte', '.md'],
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-
 	kit: {
-
-	adapter: adapter()
-
+		adapter: adapter()
 	}
 }
 

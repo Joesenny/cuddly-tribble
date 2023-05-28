81c712484a22bdfa861db493abd6956bc842ec51
joe senenfelder <est2020.joe@gmail.com>
2023-05-27 21:32:35 -0500

    page transition

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
+	import { url } from './../lib/config.ts';
     import Header from './header.svelte'
     import Footer from './footer.svelte'
+    import PageTransition from './transition.svelte'
 
     import 'open-props/style'
     import 'open-props/normalize'
     import 'open-props/buttons'
 
     import '../app.css'
+
+    export let data
 </script>
 
 <div class="layout">
     <Header />
 
     <main>
+        <PageTransition url={data.url}>
         <slot />
+        </PageTransition>
     </main>
     
     <Footer />
